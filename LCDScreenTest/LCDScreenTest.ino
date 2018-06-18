ed3de1eac3c23cb0be01d3918294321cfde7bc07
Spiri <Z1814730@students.niu.edu>
2018-06-18 08:22:50 -0500

    Testing Components

--- LCDScreenTest/LCDScreenTest.ino
+++ LCDScreenTest/LCDScreenTest.ino
@@ -46,12 +46,19 @@ void setup()
 {
   lcd.begin(16, 2);
   lcd.clear(); 
-  lcd.print("Hello World!"); 
+  lcd.print("  HELLO WORLD!"); 
+  delay(1500);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("    I GAINED ");
+  lcd.setCursor(0, 1);
+  lcd.print("  CONSCIOUSNESS");
+  delay(2000);
+  lcd.clear();
 }
 
 void loop() 
 {
   lcd.setCursor(0, 1); 
-  lcd.print(millis()/1000); 
 }
 

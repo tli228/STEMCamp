f3f8dbc0e8a63989e1a5276902d4611146318ae1
Spiri <Z1814730@students.niu.edu>
2018-08-05 10:56:14 -0500

    Made New Song

---
+++ SecretsBuzz/SecretsBuzz.ino
@@ -0,0 +1,128 @@
+int bassPin = 2; 
+
+void setup() 
+{
+  pinMode(bassPin, OUTPUT); 
+}
+
+void loop() 
+{
+
+ //Intro
+ tone(bassPin, 146.83, 300); //D
+ delay(300); 
+ tone(bassPin, 185.00, 300); //F#
+ delay(300); 
+ tone(bassPin, 220.00, 300); //A
+ delay(300); 
+ tone(bassPin, 185.00, 300); //F#
+ delay(300); 
+ tone(bassPin, 220.00, 300); //A
+ delay(300); 
+ tone(bassPin, 185.00, 300); //F#
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300); 
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 146.83, 300); //D
+ delay(300); 
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 220.00, 300); //A
+ delay(300); 
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 220.00, 300); //A
+ delay(300); 
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 146.83, 300); //D
+ delay(300); 
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 138.59, 300); //C#
+ delay(300);
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 220.00, 300); //A
+ delay(300);
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 220.00, 300); //A
+ delay(300);
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 138.59, 300); //C#
+ delay(300);
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 138.59, 300); //C#
+ delay(300);
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 220.00, 300); //A
+ delay(300);
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 220.00, 300); //A
+ delay(300);
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 138.59, 300); //C#
+ delay(300);
+ tone(bassPin, 185.00, 300); //F#
+ delay(300);
+ tone(bassPin, 123.47, 300); //B
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300); 
+ tone(bassPin, 246.94, 300); //B
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300); 
+ tone(bassPin, 246.94, 300); //B
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 123.47, 300); //B
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 246.94, 300); //B
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 98.00, 300); //G
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 246.94, 300); //B
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 246.94, 300); //B
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 123.47, 300); //B
+ delay(300);
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 98.00, 300); //G
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 196.00, 300); //G
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 196.00, 300); //G
+ delay(300); 
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+ tone(bassPin, 123.47, 300); //B
+ delay(300);
+ tone(bassPin, 146.83, 300); //D
+ delay(300);
+}

9e62d39042c105a6c8890708005fa1aab52845f7
Theresa <z1814730@students.niu.edu>
2019-02-21 16:41:45 -0600

    Added stuff to button

--- TickTock/TickTock.ino
+++ TickTock/TickTock.ino
@@ -1,20 +1,64 @@
+/*
+ * Toggle of Three Way Button Works 
+ */
 
-#include <Servo.h> 
+int ledPin = 5; 
+int ledPin2 = 7; 
 
-Servo motor; 
-int motorPin = 9; 
+int buttonPin = 12; 
+int state = HIGH; 
+int reading; 
+int prev = LOW; 
+
+int count = 0; 
 
 void setup() 
 {
-  pinMode(motorPin, OUTPUT); 
-  motor.attach(motorPin); 
+  pinMode(buttonPin, INPUT_PULLUP); 
+  pinMode(ledPin, OUTPUT); 
+  pinMode(ledPin2, OUTPUT); 
+
+  Serial.begin(9600); 
 }
 
 void loop() 
 {
-  for (int i = 0; i < 60; i++)
+  reading = digitalRead(buttonPin); 
+
+  Serial.println(reading); 
+  
+if (reading == HIGH && prev == LOW)
+{
+  if (state == HIGH && count == 0)
   {
-    motor.write(255); 
-    delay(60000); 
+    count++;
+    digitalWrite(ledPin, HIGH); 
+    digitalWrite(ledPin2, LOW);
+    state = LOW; 
   }
+  else if (state == HIGH && count == 1)
+  {
+    count++;
+    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, HIGH); 
+    state = LOW; 
+  }
+  else if (state == HIGH && count == 2)
+  {
+    count++; 
+    digitalWrite(ledPin, HIGH); 
+    digitalWrite(ledPin2, HIGH);
+    state = LOW; 
+  }
+  else if (state == HIGH && count == 3) 
+  {
+    count = 0;
+    digitalWrite(ledPin, LOW); 
+    digitalWrite(ledPin2, LOW);
+    state = LOW; 
+  }
+   state = HIGH;
+ }
+
+ prev = reading; 
 }

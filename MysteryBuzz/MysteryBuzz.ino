6fa0b36f07f2fe897ca337258f631613bcd56fe0
Theresa <z1814730@students.niu.edu>
2019-07-06 17:47:58 -0500

    Fav Show

--- MysteryBuzz/MysteryBuzz.ino
+++ MysteryBuzz/MysteryBuzz.ino
@@ -8,117 +8,432 @@ int buzzerPin = 9;
 void setup() 
 {
   pinMode(buzzerPin, OUTPUT); 
+
+  tone(buzzerPin, 700, 500); //F
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  
+  delay(3000); 
+  
+  tone(buzzerPin, 700, 500); //A
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  
+  delay(3000);
+  
+  tone(buzzerPin, 700, 500); //V
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  
+  delay(3000);
+  
+  tone(buzzerPin, 700, 500); //S
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  
+  delay(3000);
+
+  tone(buzzerPin, 700, 500); //H
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  
+  delay(3000);
+
+  tone(buzzerPin, 700, 500); //O
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(3000);
+
+  tone(buzzerPin, 700, 500); //W
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500);  
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 200, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+  tone(buzzerPin, 700, 500);
+  delay(500); 
+  noTone(buzzerPin); 
+  delay(500); 
+
+  //end
+  tone(buzzerPin, 800, 500); 
+  delay(500);
+  tone(buzzerPin, 900, 500); 
+  delay(500); 
+  tone(buzzerPin, 1000, 500); 
+  delay(500); 
+  noTone(buzzerPin); 
 }
 
 void loop() 
 {
-  tone(buzzerPin, 700, 500); //F
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-
-  delay(1000); 
-  
-  tone(buzzerPin, 700, 500); //A
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-
-  delay(1000); 
-  
-  tone(buzzerPin, 700, 500); //V
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-
-  delay(3000); 
-
-  tone(buzzerPin, 700, 500); //S
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-
-  delay(1000); 
-
-  tone(buzzerPin, 700, 500); //H
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-
-  delay(1000); 
-
-  tone(buzzerPin, 700, 500); //O
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-
-  delay(1000); 
-
-  tone(buzzerPin, 700, 500); //W
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 200, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
-  delay(500); 
-  tone(buzzerPin, 700, 500);
+//  tone(buzzerPin, 700, 500); //F
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  
+//  delay(3000); 
+//  
+//  tone(buzzerPin, 700, 500); //A
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  
+//  delay(3000);
+//  
+//  tone(buzzerPin, 700, 500); //V
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  
+//  delay(3000);
+//  
+//  tone(buzzerPin, 700, 500); //S
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  
+//  delay(3000);
+//
+//  tone(buzzerPin, 700, 500); //H
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  
+//  delay(3000);
+//
+//  tone(buzzerPin, 700, 500); //O
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(3000);
+//
+//  tone(buzzerPin, 700, 500); //W
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500);  
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 200, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(500); 
+//  tone(buzzerPin, 700, 500);
+//  delay(500); 
+//  noTone(buzzerPin); 
+//  delay(3000); 
 }

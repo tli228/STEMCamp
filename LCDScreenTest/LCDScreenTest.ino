de4bae3cc45cc17d6adc79f55c130c7163e2f036
Theresa <z1814730@students.niu.edu>
2019-07-17 13:51:47 -0500

    LCD Screen for sure works

--- LCDScreenTest/LCDScreenTest.ino
+++ LCDScreenTest/LCDScreenTest.ino
@@ -1,50 +1,10 @@
-/*#include <Wire.h>
-#include <LiquidCrystal_PCF8574.h>
-
-LiquidCrystal_PCF8574 lcd(0x3f);
-
-void setup() 
-{
-  Wire.begin();
-  Wire.beginTransmission(0x3f);
-  
-  lcd.begin(16, 2); 
-
-  lcd.setBacklight(255);
-  lcd.home(); 
-  lcd.clear();
-  delay(300);
-  lcd.setCursor(0, 0);
-  lcd.print(" FOOLISH MORTAL!");
-  delay(1500);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("   I AM YOUR");
-  lcd.setCursor(0, 1);
-  lcd.print("  CONTROLLER!!!");
-  delay(2000);
-  lcd.clear();
-  
-  lcd.setCursor(0, 0);
-  lcd.print("   NIRV  2018");
-  lcd.setCursor(0, 1);
-  lcd.print("   Controller");
-  delay(1000);
-}
-
-void loop()
-{
-  
-}*/
-
-//Didn't work the first time, needs a pot 
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() 
 {
-  lcd.begin(16, 2);
+  lcd.begin(16, 2); //16 pixels in a row and 2 pixels in a column 
   lcd.clear(); 
   lcd.print("  HELLO WORLD!"); 
   delay(1500);
@@ -61,4 +21,3 @@ void loop()
 {
   lcd.setCursor(0, 1); 
 }
-
